fd6eb6c5f0c35e81b7b7501ac416e7e6c629361a
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2021-05-23 10:10:19 +0800

    Implement navbar for mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,99 +1,58 @@
 import { useCart } from "contexts/CartContext";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 
-const _NavBar = styled.nav`
-  display: flex;
-  justify-content: center;
+import { FiMenu } from "react-icons/fi";
 
-  /* background-color: #f2efe2; */
-  background-color: var(--color-primary-dark);
-  color: var(--color-on-primary);
-  width: 100%;
-  font-size: 1rem;
-
-  ul {
-    list-style: none;
-    align-items: center;
-    display: flex;
-    max-width: 960px;
-    flex-grow: 1;
-    li.link {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-      &:hover {
-        transition: all 0.3s ease;
-        background-color: var(--color-secondary);
-        a {
-          color: var(--color-on-secondary);
-        }
-        .badge {
-          background-color: var(--color-primary);
-          color: var(--color-on-primary);
-        }
-      }
-      a {
-        text-decoration: none;
-        color: var(--color-on-primary);
-        padding: 20px;
-      }
-      .badge {
-        margin-left: 10px;
-        display: inline-block;
-        width: 24px;
-        background-color: var(--color-secondary);
-        border-radius: 11px;
-        font-size: 0.8rem;
-        color: var(--color-on-secondary);
-        text-align: center;
-      }
-    }
-    li.brand {
-      a {
-        padding: 10px;
-      }
-      img {
-        max-height: 40px;
-      }
-    }
-    li.spacer {
-      flex-grow: 1;
-    }
-  }
-`;
+import {
+  _NavBar,
+  NavMenu,
+  NavBrand,
+  NavList,
+  NavItem,
+  Badge,
+  Spacer,
+  NavMenuIcon,
+} from "./NavBar.styles";
 
 const NavBar = () => {
   const { cart } = useCart();
-
+  const [showList, setShowList] = useState();
   const count = cart.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <_NavBar>
-      <ul>
-        <li className="link brand">
+      <NavMenu>
+        <NavBrand>
           <Link to="/">
             <img alt="Carved Rock Fitness" src="/images/logo-white.png" />
-            {/* Carved Rock Fitness */}
           </Link>
-        </li>
-        <li className="link">
-          <Link to="/shoes">Shoes</Link>
-        </li>
-        <li className="link">
-          <Link to="/backpacks">Backpacks</Link>
-        </li>
-        <li className="link">
-          <Link to="/cart">
-            Cart
-            <span className="badge">{count}</span>
-          </Link>
-        </li>
-        <li className="spacer"></li>
-        <li className="link">
-          <a href="#!">Login</a>
-        </li>
-      </ul>
+        </NavBrand>
+
+        <NavList show={showList} onClick={() => setShowList(false)}>
+          <NavItem>
+            <Link to="/shoes">Shoes</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/backpacks">Backpacks</Link>
+          </NavItem>
+          <NavItem>
+            <Link to="/cart">
+              Cart
+              <Badge>{count}</Badge>
+            </Link>
+          </NavItem>
+          <Spacer></Spacer>
+          <NavItem>
+            <a href="#!">Login</a>
+          </NavItem>
+        </NavList>
+
+        <NavMenuIcon>
+          <a onClick={() => setShowList(!showList)}>
+            <FiMenu />
+          </a>
+        </NavMenuIcon>
+      </NavMenu>
     </_NavBar>
   );
 };
---
+++ src/components/NavBar/NavBar.styles.jsx
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+export const _NavBar = styled.nav`
+  display: flex;
+  justify-content: center;
+
+  background-color: var(--color-primary-dark);
+  color: var(--color-on-primary);
+
+  width: 100%;
+  font-size: 1rem;
+`;
+
+export const NavList = styled.ul`
+  list-style: none;
+  align-items: center;
+  display: flex;
+  max-width: 960px;
+  flex-grow: 1;
+  /* justify-content: space-between; */
+
+  @media screen and (max-width: 720px) {
+    flex-direction: column;
+    flex-grow: 1;
+    position: fixed;
+    left: ${(props) => (props.show ? 0 : "-100vw")};
+    top: 60px;
+    width: 100vw;
+    height: calc(100vh - 60px);
+    align-items: stretch;
+    background-color: var(--color-primary-dark);
+    transition: 0.3s all ease;
+    z-index: 1500;
+  }
+`;
+
+export const Spacer = styled.li`
+  flex-grow: 1;
+`;
+export const NavMenu = styled.div`
+  list-style: none;
+  align-items: center;
+  display: flex;
+  max-width: 960px;
+  flex-grow: 1;
+  justify-content: space-between;
+`;
+
+export const NavItem = styled.li`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  text-align: center;
+  &:hover {
+    transition: all 0.3s ease;
+    background-color: var(--color-secondary);
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color-on-primary);
+    padding: 20px;
+    width: 100%;
+  }
+`;
+
+export const NavBrand = styled(NavItem)`
+  a {
+    padding: 10px;
+  }
+  img {
+    max-height: 40px;
+  }
+`;
+
+export const NavMenuIcon = styled(NavItem)`
+  @media screen and (min-width: 720px) {
+    display: none;
+  }
+`;
+
+export const Badge = styled.span`
+  margin-left: 10px;
+  display: inline-block;
+  width: 24px;
+  background-color: var(--color-secondary);
+  border-radius: 11px;
+  font-size: 0.8rem;
+  color: var(--color-on-secondary);
+  text-align: center;
+
+  &:hover {
+    background-color: var(--color-primary);
+    color: var(--color-on-primary);
+  }
+`;
